0d8f752d17c7fc2b1648ad4abb2938c8b10f1456
Alexandre Henrique R Araujo <64115785+alexandre-henrique-rp@users.noreply.github.com>
2025-10-06 14:03:14 -0300

    🔨 chore: Habilita CORS para todas as origens
    - Adiciona configuração CORS para permitir qualquer origem, métodos e headers

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,13 @@ import { AppModule } from './app.module';
 const PORT = process.env.PORT || 3000;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
+  
   await app.listen(PORT, () => {
     console.log(`Application is running on: http://localhost:${PORT}`);
   });
